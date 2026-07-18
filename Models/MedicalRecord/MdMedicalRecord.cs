92de9ab0cb49d4cdd5e4ef72ae15441fe22dd469
Virgile <agbowagliv@gmail.com>
2026-07-18 02:59:24 +0000

    Add medical records management feature
    Introduced a new page (`MedicalRecord.razor`) for managing medical records, including viewing, creating, editing, and deleting records. Added a statistics section, searchable table, and detailed modal for record management.
    
    Updated `Patient.razor` to enable navigation to the medical records page for specific patients. Enhanced appointment and patient count logic to support filtered views.
    
    Added new data models (`MdPostMedicalRecord` and `MdGetMedicaRecord`) to handle medical record data. Defined extensive CSS for UI elements and implemented Blazor code-behind logic for managing records.

---
+++ Models/MedicalRecord/MdMedicalRecord.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace HospitalManagementSystem.Models.MedicalRecord
+{
+    public class MdPostMedicalRecord
+    {
+        public string? PatientId { get; set; }
+        public DateTime RecordDate { get; set; } = DateTime.Now;
+        public string? Temperature { get; set; }
+        public string? Pulse { get; set; }
+        public string? BloodPressure { get; set; }
+        public string? Weight { get; set; }
+        public string? Height { get; set; }
+        public string? BloodGroup { get; set; }
+        public string? DoctorId { get; set; }
+        public string? AppointmentId { get; set; }
+        public string? Symptoms  { get; set; }
+        public string? Diagnosis { get; set; }
+        public string? Note { get; set; }
+        public string? Treatment { get; set; }
+        public string? Prescription { get; set; }
+        public string? LaboratoryTest { get; set; }
+        public DateTime? NextVisit { get; set; }
+        public string? Remarks { get; set; }
+
+    }
+
+    public class MdGetMedicaRecord : MdPostMedicalRecord
+    {
+
+        public string? Id { get; set; }
+        public string? PatientName { get; set; }
+        public string? DoctorName { get; set; }
+        public string? AppointmentName { get; set; }
+        public string? PatientNumber { get; set; }
+
+
+
+    }
+}
